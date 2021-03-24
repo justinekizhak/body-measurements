1969f42640bd242f2f1ede26e509d3e21180f377
Justine Kizhakkinedath <justine@kizhak.com>
2021-03-24 21:48:28 +0530

    fix: ts and eslint error

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid h-screen grid-rows-3 p-8">
     <div class="text-4xl font-bold break-words">Body Measurements</div>
-    <div ref="tripetto" class="sm:w-1/2"></div>
+    <div ref="form" class=""></div>
     <div class="flex items-end">
       <a
         href="https://justine.kizhak.com"
@@ -39,15 +39,17 @@ export default Vue.extend({
   },
   methods: {
     runTripetto() {
+      // @ts-ignore
       // eslint-disable-next-line no-undef
       const tripetto = TripettoServices.init({
         token:
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiMkFpUGR2Tm81dUM2REhaSEdqNHVTT1lzUndJam4vVXJmVTREd2M3cFZyQT0iLCJkZWZpbml0aW9uIjoiSnpHTEFXOGt2eGFNYzJ5VGFQMXY2K0JIaEhoNXUvOENqRERlZEEzbmpRbz0iLCJ0eXBlIjoiY29sbGVjdCJ9.dW6Q-zYb8jyyi8qfWpZ2f0CI82RyyAY01yUuGX_bC7I',
       })
 
+      // @ts-ignore
       // eslint-disable-next-line no-undef
       TripettoAutoscroll.run({
-        element: this.$refs.tripetto,
+        element: this.$refs.form,
         definition: tripetto.definition,
         styles: tripetto.styles,
         l10n: tripetto.l10n,
