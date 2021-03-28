d4b285611cfcf496ef882decddf4c205bb6c8010
Justine Kizhakkinedath <justine@kizhak.com>
2021-03-28 13:31:27 +0530

    fix: build error

--- components/AppLogin.vue
+++ components/AppLogin.vue
@@ -19,6 +19,7 @@ export default Vue.extend({
         .then((result) => {
           const credential = result.credential
           // This gives you a Google Access Token. You can use it to access the Google API.
+          // @ts-ignore
           const token = credential?.accessToken
           // The signed-in user info.
           const user = result?.user
