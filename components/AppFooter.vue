b59acd789b95bfe4be45d6fd16395fa69b857a96
Justine Kizhakkinedath <justine@kizhak.com>
2021-04-04 01:30:47 +0530

    feat: add logo; add font

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,6 +5,19 @@
     class="border-b-2 border-gray-300"
   >
     <span> Created by </span>
-    <span class="ml-1 font-bold"> Justine Kizhakkinedath </span>
+    <span class="ml-1 font-bold tracking-widest uppercase font-anurati">
+      JustineKizhakkinedath
+    </span>
   </a>
 </template>
+
+<style scoped>
+@font-face {
+  font-family: anurati;
+  src: url('/fonts/Anurati-Regular.otf');
+}
+
+.font-anurati {
+  font-family: anurati, sans-serif;
+}
+</style>
--- components/AppHeader/index.vue
+++ components/AppHeader/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="z-10 flex flex-col justify-between gap-4 lg:gap-0 lg:flex-row ribbon"
+    class="z-10 flex flex-col items-center justify-between gap-4 lg:gap-0 lg:flex-row ribbon"
   >
     <div
-      class="text-3xl font-bold break-words cursor-pointer lg:text-5xl"
+      class="text-3xl font-bold tracking-widest uppercase break-words cursor-pointer lg:text-5xl font-anurati"
       @click="$router.push('/')"
     >
       Body Measurements
@@ -20,5 +20,13 @@
   top: 7px;
   right: -20px;
   @apply bg-red-700 fixed  font-display uppercase font-bold transform-gpu rotate-45 text-xs leading-none flex justify-center items-center;
+}
+@font-face {
+  font-family: anurati;
+  src: url('/fonts/Anurati-Regular.otf');
+}
+
+.font-anurati {
+  font-family: anurati, sans-serif;
 }
 </style>
