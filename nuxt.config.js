1a1b80790db1006acdf52b7e7e6a875abe903d2f
Justine Kizhakkinedath <justine@kizhak.com>
2021-04-04 13:17:16 +0530

    fix: icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: description },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
       {
         src: 'https://unpkg.com/tripetto-runner-foundation',
